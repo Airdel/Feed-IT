931fb2d74a96c04096949a65dfedf2fb57e27bfc
Miramontes Tiznado Enrique Alonso <Enalmiramontesti@ittepic.edu.mx>
2021-10-11 02:05:28 -0600

    Se agrego el analizador lexico
    -Miramontes Tiznado Enrique Alonso

---
+++ Feed-IT/src/analizador_lexico/Tokens.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package analizador_lexico;
+
+/**
+ *
+ * @author Equipo 5 FEEDIT
+ */
+public enum Tokens {
+    Reserved,
+    Asignacion,
+    Igual,
+    ExactamenteIgual,
+    Suma,
+    Resta,
+    Multiplicacion,
+    Division,
+    Potencia,
+    Modulo,
+    MayorQue,
+    MenorQue,
+    MayorIgualQue,
+    MenorIgualQue,
+    DiferenteDe,
+    operadorLogicoAND,
+    operadorLogicoOR,
+    operadorLogicoNOT,
+    Parentesis_I,
+    Parentesis_D,
+    Salto_Linea,
+    Identifier,
+    Number,
+    ERROR
+}//Tokens
