9c66967c760a793d1ecef7530402d1ab8206d0e4
Darien Márquez <darien1005@hotmail.com>
2021-12-02 00:07:50 -0700

    Clase manejo de errores & pequeña adicion sintax.cup

--- Feed-IT/src/analizador_sintactico/C_Errores.java
+++ Feed-IT/src/analizador_sintactico/C_Errores.java
@@ -1,15 +1,52 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+//FEED-IT
+//Constructores para la gestión de Errores Sintácticos.
 package analizador_sintactico;
 
-/**
- * Airdel.
- * @author darie
- */
 public class C_Errores {
 
+    public String Descripcion;
+    public int linea;
+    public int columna;
+
+    public C_Errores(String Descripcion){
+        this.Descripcion = Descripcion;
+    }
+    
+    public C_Errores(String Descripcion, int linea,int columna) {
+        this.Descripcion = Descripcion;
+        this.linea = linea;
+        this.columna = columna;
+    }
+
+    public C_Errores() {
+        this.Descripcion = "";
+        this.linea = 0;
+        this.columna = 0;
+    }
+
+    ///métodos get y set para la obtención del error y su ubicación en el editor.
+        
+    public String getDescripcion() {
+        return Descripcion;
+    }
+
+    public void setDescripcion(String Descripcion) {
+        this.Descripcion = Descripcion;
+    }
+
+    public int getLinea() {
+        return linea;
+    }
+
+    public void setLinea(int linea) {
+        this.linea = linea;
+    }
+    
+    public int getColumna() {
+        return columna;
+    }
+
+    public void setColumna(int columna) {
+        this.columna = columna;
+    }
 }
